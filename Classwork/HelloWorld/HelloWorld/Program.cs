dda8152150fd1357a1200f968ca75dec3cb62596
matthew.mcnatt <matthew.mcnatt@NEITD-123521>
2019-01-16 21:44:35 -0600

    commit

---
+++ Classwork/HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Multi Line comment
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    //Single line comment
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            string name;
+            Console.WriteLine("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Hello ");
+            Console.Write(name);
+        }
+    }
+}

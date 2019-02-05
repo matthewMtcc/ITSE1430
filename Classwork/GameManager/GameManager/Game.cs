5b7c3efeb1cd1682daafdebfe040e3421a1852ff
matthew.mcnatt <matthew.mcnatt@NEITD-123521>
2019-02-04 21:37:34 -0600

    Classwork in progress

---
+++ Classwork/GameManager/GameManager/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameManager
+{
+    /// <summary> Represents a game </summary>
+    public class Game
+    {
+       /// <summary> Name of the game </summary>
+       public string Name;
+       
+       public string Publisher;
+       public decimal Price;
+       public bool Owned;
+       public bool Completed;
+    }
+}

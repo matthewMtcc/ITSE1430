bae7c557e8c3481bd382bfbda764644fb7f86891
matthew.mcnatt <matthew.mcnatt@my.tccd.edu>
2019-03-10 17:54:48 -0500

    The start of Lab 2

---
+++ Labs/CharacterCreator.Winforms/CharacterCreator.Winforms/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CharacterCreator.Winforms
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnFileExit( object sender, EventArgs e )
+        {
+            var DOOM = new Character();
+            Close();
+        }
+
+        private void OnAboutClicked( object sender, EventArgs e )
+        {
+            var form = new AboutBox1();
+            form.ShowDialog();
+        }
+    }
+}

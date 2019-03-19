f4e3365bdc09ff50251882fe30627dfd95f6b8c3
matthew.mcnatt <matthew.mcnatt@NEITD-123521>
2019-03-18 21:28:28 -0500

    Classwork

--- Classwork/GameManager/GameManager/GameDataBase.cs
+++ Classwork/GameManager/GameManager/GameDataBase.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//using System.Collections.ObjectModel; //for collectiion
+
 
 namespace GameManager
 {
@@ -11,23 +13,38 @@ namespace GameManager
 
         public GameDatabase()
         {
-            var game = new Game();
-            game.Name = "DOOM";
-            game.Description = "Space Marine";
-            game.Price = 49.99M;
-            Add(game);
+            //var game = new Game();
+            //game.Name = "DOOM";
+            //game.Description = "Space Marine";
+            //game.Price = 49.99M;
 
-            game = new Game();
-            game.Name = "Oblivion";
-            game.Description = "Medieval";
-            game.Price = 89.99M;
-            Add(game);
+            //Object initializer
+            //var game = new Game() {
+            //    Name = "DOOM",
+            //    Description = "Space Marine",
+            //    Price = 49.99M
+            //};
+            //Add(game);
 
-            game = new Game();
-            game.Name = "Fallout 76";
-            game.Description = "Failed MMO";
-            game.Price = 0.01M;
-            Add(game);
+            //game = new Game() { Name = "Oblivion", Description = "Medieval", Price = 89.99M };
+            //Add(game);
+
+            //Add(new Game() {
+            //    Name = "Fallout 76",
+            //    Description = "Failed MMO",
+            //    Price = 0.01M
+            //});
+
+            //Collection initializer
+            var games = new []
+                {
+                    new Game() { Name = "DOOM", Description = "Space Marine", Price = 49.99M },
+                    new Game() { Name = "Oblivion", Description = "Medieval", Price = 89.99M },
+                    new Game() { Name = "Fallout 76", Description = "Failed MMO", Price = 0.01M }
+                };
+
+            foreach (var game in games)
+                Add(game);
         }
 
         public Game Add( Game game )
@@ -38,29 +55,30 @@ namespace GameManager
 
             //Game must be valid
             if (!game.Validate())
-                throw new Exception("Game is invalid");
+                throw new Exception("Game is invalid.");
 
             //Game names must be unique
             var existing = GetIndex(game.Name);
             if (existing >= 0)
                 throw new Exception("Game must be unique.");
 
-            //Dummy test
-            if (String.Compare(game.Name, "Anthem", true) == 0)
-                throw new InvalidOperationException("Only good games are allowed here.");
-            if (game.Price > 1000)
-                throw new NotImplementedException();
+            ////Playing around with different exceptions
+            //if (String.Compare(game.Name, "Anthem", true) == 0)
+            //    throw new InvalidOperationException("Only good games are allowed here.");
+            //if (game.Price > 1000)
+            //    throw new NotImplementedException();
 
-
-            for (var index = 0; index < _items.Length; ++index)
-            {
-                if (_items[index] == null)
-                {
-                    game.Id = ++_nextId;
-                    _items[index] = Clone(game);
-                    break;
-                };
-            };
+            //for (var index = 0; index < _items.Length; ++index)
+            //{
+            //    if (_items[index] == null)
+            //    {
+            //        game.Id = ++_nextId;
+            //        _items[index] = Clone(game);
+            //        break;
+            //    };
+            //};
+            game.Id = ++_nextId;
+            _items.Add(Clone(game));
 
             return game;
         }
@@ -91,14 +109,15 @@ namespace GameManager
             return game;
         }
 
-        public void Delete( int id)
+        public void Delete( int id )
         {
             if (id <= 0)
-                throw new ArgumentOutOfRangeException(nameof(id), "Id must be greater then 0");
+                throw new ArgumentOutOfRangeException(nameof(id), "Id must be > 0.");
 
             var index = GetIndex(id);
             if (index >= 0)
-                _items[index] = null;
+                _items.RemoveAt(index);
+            //_items[index] = null;
         }
 
         public Game Get( int id )
@@ -115,19 +134,22 @@ namespace GameManager
 
         public Game[] GetAll()
         {
-            //How many games?
-            int count = 0;
+            ////How many games?
+            //int count = 0;
+            //foreach (var item in _items)
+            //    if (item != null)
+            //        ++count;
+
+            //var tempIndex = 0;
+            //var temp = new Game[count];
+            //for (var index = 0; index < _items.Length; ++index)
+            //    if (_items[index] != null)
+            //        temp[tempIndex++] = Clone(_items[index]);
+            var temp = new List<Game>();
             foreach (var item in _items)
-                if (item != null)
-                    ++count;
+                temp.Add(Clone(item));
 
-            var tempIndex = 0;
-            var temp = new Game[count];
-            for (var index = 0; index < _items.Length; ++index)
-                if (_items[index] != null)
-                    temp[tempIndex++] = Clone(_items[index]);
-
-            return temp;
+            return temp.ToArray();
         }
 
         private void Clone( Game target, Game source )
@@ -149,18 +171,30 @@ namespace GameManager
         }
         private int GetIndex( int id )
         {
-            for (var index = 0; index < _items.Length; ++index)
+            for (var index = 0; index < _items.Count; ++index)
                 if (_items[index]?.Id == id)
                     return index;
 
             return -1;
         }
 
-        private readonly Game[] _items = new Game[100];
+
+        //Arrays are so 90s
+        //private readonly Game[] _items = new Game[100];
+
+        //ArrayLists are so 00s
+        //private readonly ArrayList _items = new ArrayList();
+
+        private readonly List<Game> _items = new List<Game>();
+        //private readonly Collection<Game> _items = new Collection<Game>();
+        //clear();
+        //contains(); 
+
         private int _nextId = 0;
+
         private int GetIndex( string name )
         {
-            for (var index = 0; index < _items.Length; ++index)
+            for (var index = 0; index < _items.Count; ++index)
                 if (String.Compare(_items[index]?.Name, name, true) == 0)
                     return index;
 
